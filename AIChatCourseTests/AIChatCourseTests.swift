28d6ba393ddc9dbf6eae72c663f72c196dd58d21
ShreyaDevX <shreya@co-pr.com>
2025-11-22 17:37:39 +1100

    Initial commit

---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Shreya Pallan on 22/11/25.
+//
+
+import Testing
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
