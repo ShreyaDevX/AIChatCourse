5df7f6b1682a5e989576ec69e49204aa1c8da4a6
ShreyaDevX <shreya@co-pr.com>
2025-12-25 17:26:00 +1100

    Adding WelcomeView

---
+++ AIChatCourse/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  AIChatCourse
+//
+//  Created by Shreya Pallan on 25/12/25.
+//
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+    static let privacyPolicy = "https://www.apple.com"
+    static let termsOfServiceUrl = "https://www.apple.com"
+
+}
